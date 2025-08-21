bd89ed80a0b8f98ded4477e2101356218e2227ea
pramudithan <pramudithanadun@gmail.com>
2025-08-21 10:24:07 +0530

    Added FE URL

--- mern-backend/src/index.ts
+++ mern-backend/src/index.ts
@@ -32,7 +32,12 @@ const app = express();
 //     credentials: true,
 //   })
 // );
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://angel-paradise.vercel.app", // replace with your actual frontend URL
+    credentials: true,
+  })
+);
 app.use(express.json());
 // Serve uploaded images statically
 app.use("/uploads", express.static(path.join(__dirname, "../../uploads")));
