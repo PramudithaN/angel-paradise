1113566cacb95e87269921cfce0243104dc76c8b
pramudithan <pramudithanadun@gmail.com>
2025-08-21 10:08:35 +0530

    Fixed deployed cors error

--- mern-backend/src/index.ts
+++ mern-backend/src/index.ts
@@ -21,6 +21,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:3000",
       "http://localhost:5174",
+      "https://your-frontend.vercel.app",
     ],
     credentials: true,
   })
